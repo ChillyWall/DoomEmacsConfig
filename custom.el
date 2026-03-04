b56068d6663238a018d0587a6331707447d879b8
chilly <chillywall@163.com>
2026-03-04 23:44:36 +0800

    自定义org部分显示效果控制变量

--- custom.el
+++ custom.el
@@ -8,7 +8,13 @@
  '(custom-safe-themes
    '("8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a"
      "fffef514346b2a43900e1c7ea2bc7d84cbdd4aa66c1b51946aade4b8d343b55a"
-     "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" default)))
+     "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" default))
+ '(org-appear-autolinks t)
+ '(org-format-latex-options
+   '(:foreground default :background default :scale 1.2 :html-foreground "Black"
+     :html-background "Transparent" :html-scale 1.0 :matchers
+     ("begin" "$1" "$" "$$" "\\(" "\\[")))
+ '(org-pretty-entities nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
